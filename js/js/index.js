c1a350a1fd29e579200040a628944fbd1aac30f0
naif <nayef7138@gmail.com>
2023-09-24 09:59:53 +0300

    update: js course

---
+++ js/js/index.js
@@ -0,0 +1,26 @@
+// console.log("hello world! from index.js", 1, 2, 3);
+
+// console.log(prompt('enter your password'))
+// console.log(confirm('are you above 16'))
+
+var name = "mohammad"; // string
+var age = 20; // number
+var skills = ["html", "css"]; // array
+var cardInfo = { // object
+  name: "TCard",
+  color: "red",
+  made: "china",
+};
+
+var isMobileDev = true  // boolean
+
+var myInfo
+
+
+console.log({
+  name,
+  age,
+  skills,
+  cardInfo,
+  myInfo
+});
