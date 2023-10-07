d30ae165684283ed5d6fd1fe8042ed62c16be05d
Naif Sameer <nayef7138@gmail.com>
2023-10-07 10:20:46 +0300

    update: js

---
+++ js/js/api.js
@@ -0,0 +1,189 @@
+// let order = (call_production) => {
+//   setTimeout(() => {
+//     call_production();
+//   }, 1000);
+// };
+
+// let production = () => {
+//   setTimeout(() => {
+//     console.log("step 1");
+//     setTimeout(() => {
+//       console.log("step 2");
+//       setTimeout(() => {
+//         console.log("step 3");
+//         setTimeout(() => {
+//           console.log("step 4");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// };
+
+// order(production);
+
+// const last = () => {
+//   setTimeout(() => {
+//     console.log("last 2000");
+//   }, 2000);
+// };
+
+// const first = () => {
+//   const prodcuts = [200];
+
+//   setTimeout(() => {
+//     prodcuts.push("pizza");
+//     console.log("first timeout 1000");
+
+//     prodcuts.forEach((item) => {
+//       document.body.innerHTML = item;
+//       console.log(item, "item in loop");
+//     });
+
+//     second();
+//   }, 1000);
+
+//   //   for (let index = 0; index < 1; index++) {
+//   //     console.log("first loop");
+//   //   }
+// };
+
+// const second = () => {
+//   setTimeout(() => {
+//     console.log("second timeout 3000");
+
+//     last();
+//   }, 3000);
+// };
+
+// first();
+
+// const loginUser = (username) => {
+//   return new Promise((resolveThisCode, rejectThisData) => {
+//     if (username === "ali")
+//       resolveThisCode({
+//         name: "ali ali",
+//         message: "success",
+//         username,
+//       });
+//     else rejectThisData("fail");
+//   });
+// };
+
+// loginUser("ali")
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err, "error");
+//   });
+
+const first = (intValue) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("first", intValue);
+      resolve(intValue);
+    }, 300);
+  });
+};
+
+const second = (intValue) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("second", intValue);
+
+      resolve(10 + intValue);
+      //   reject("second reject");
+      console.log("code .");
+    }, 200);
+  });
+};
+
+const third = (intValue) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("third", intValue);
+      resolve(intValue + 100);
+    }, 100);
+  });
+};
+
+// first(1)
+//   .then((data) => {
+//     console.log('first is ', data)
+//    return second(data)
+//   })
+//   .then((data) => {
+//     console.log('second is ', data)
+//    return third(data)
+//   })
+// //   .then(second)
+// //   .then(third)
+//   .then((data) => {
+//     console.log('third is',data);
+//   });
+
+// first(1)
+//   .then(second)
+//   .catch((d) => {
+//     console.log(d);
+//     return 0;
+//   })
+//   .then(third)
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//   method: "get",
+// })
+//     .then((response) => {
+//       const json  = response.json()
+
+//     //   console.log(json)
+//     //   json.then((data) => console.log(data))
+
+//       return json;
+//     })
+//     .then((data) => console.log(data))
+// //   .then((res) => res.json())
+// //   .then((json) => console.log(json));
+
+// const data = {
+//     count: 0,
+//     inc(sum = 1) {
+//         this.count += sum;
+//         console.log(this)
+
+//         return this
+//     }
+// }
+
+// console.log(data.count)
+// data.inc(100)
+
+// console.log(data.count)
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "get",
+})
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.innerHTML = `
+        <div>
+            <h1>${data.title}</h1>
+            <p>${data.body}</p>
+        </div>
+    `
+  });
+
+
+  const readFiles =  (files) => {
+    return new Promise((resolve, reject) => {
+        // read files 
+        resolve(files)
+    }
+    )
+  }
+
+
+
+readFiles([]).then((data) => {
+
+})
